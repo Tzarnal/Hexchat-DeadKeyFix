fe6b13784ff1408a6fdc187fafdafa9d27135f8c
Xesyto <M8R-acpfdp@mailinator.com>
2013-09-15 15:06:51 +0200

    Initial Import

---
+++ DeadKeyFix.py
@@ -0,0 +1,28 @@
+import xchat as hexchat
+import re
+
+__module_name__ = "keyfix"
+__module_version__ = "1.0"
+__module_description__ = "Fixes the stupid Us-International deadkey issue"
+
+checkP = re.compile("¨|´")
+replaceP1 = re.compile("¨")
+replaceP2 = re.compile("´")
+
+def keypress_cb(word, word_eol, userdata):
+	inputtext = hexchat.get_info('inputbox')
+	
+	m = checkP.search(inputtext)
+	if m:
+		inputtext = replaceP1.sub('"',inputtext)
+		inputtext = replaceP2.sub("'",inputtext)
+		hexchat.command('settext {}'.format(inputtext))
+		hexchat.command('setcursor {}'.format(len(inputtext)))
+
+def unload_cb(userdata):
+	print(__module_name__, 'version', __module_version__, 'unloaded.')
+
+hexchat.hook_print('Key Press', keypress_cb) 
+hexchat.hook_unload(unload_cb)
+
+print(__module_name__, 'version', __module_version__, 'loaded.')
